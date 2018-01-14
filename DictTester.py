61aa9532be1503a0e0d390c55eed448bbee75491
raulverma <raulverma@hotmail.com>
2018-01-13 17:35:16 -0800

    Adding Dictionary Test Python Code

---
+++ DictTester.py
@@ -0,0 +1,5 @@
+subjects = {"Biology":"Life Science","Chemistry":"Physical Science","EarthScience":"Earth & Space Science","Energy":"Energy","Math":"Math","Physics":"Physical Science","SpaceScience":"Earth & Space Science"}
+
+for key in subjects:
+    print (subjects[key])
+    print (key+".txt")
